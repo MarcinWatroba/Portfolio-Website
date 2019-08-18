639c523e77c3209f1552c119e01c00ff24e3b67a
marcin388 <p14170863@my365.dmu.ac.uk>
2019-08-19 00:20:45 +0100

    hotfix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express    = require("express")
       LocalStrategy = require("passport-local"),
       Portfolio  = require("./models/portfolio"),
       User     = require("./models/user");
- //      seedDB     = require("./seed");
+       seedDB     = require("./seed");
 
 
 const url = process.env.PORTFOLIODB || "mongodb://localhost:27017/portfolio";
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
     next();
 });
 
- //seedDB();
+ seedDB();
 
  isLoggedIn = function(req, res, next) {
     if(req.isAuthenticated()){
